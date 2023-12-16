a34de41463bd627ac58f7e8ea5a9ef4ec8cb12cb
Sanyajaiswal <sanya26mobcoder@gmail.com>
2023-12-16 20:06:54 +0530

    Ready to commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $(".nav-toggle").click(function(){
+      $(".collapse-toggle").toggle("slow");
+      $('body').toggleClass('toggle-effect');
+      });
+
+      $( window ).on( "scroll", function() {
+        console.log('window',window.scrollY)
+        if(window.scrollY>350){
+          $('.navbar_header').addClass('fix_header');
+        }else{
+          $('.navbar_header').removeClass('fix_header');
+        }
+    } );
+    });
