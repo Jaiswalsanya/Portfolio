9d7de354aab621c1d6bf2aa7065c469bcbe6d546
Sanyajaiswal <sanya26mobcoder@gmail.com>
2023-12-16 23:59:02 +0530

    Updated file

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".nav-toggle").click(function () {
+    $(".collapse-toggle").toggle("slow");
+    $("body").toggleClass("toggle-effect");
+  });
 
-    $(".nav-toggle").click(function(){
-      $(".collapse-toggle").toggle("slow");
-      $('body').toggleClass('toggle-effect');
-      });
-
-      $( window ).on( "scroll", function() {
-        console.log('window',window.scrollY)
-        if(window.scrollY>350){
-          $('.navbar_header').addClass('fix_header');
-        }else{
-          $('.navbar_header').removeClass('fix_header');
-        }
-    } );
-    });
+  $(window).on("scroll", function () {
+    console.log("window", window.scrollY);
+    if (window.scrollY > 350) {
+      $(".navbar_header").addClass("fix_header");
+    } else {
+      $(".navbar_header").removeClass("fix_header");
+    }
+  });
+});
